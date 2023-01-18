af9e08265e45c91a91748525d16743ef1049951d
Pintilie <roxana.pintilie28@gmail.com>
2023-01-18 21:54:30 +0200

    rfgdcl

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,15 +1,18 @@
-import Head from 'next/head'
-
+import { Layout } from '@/layouts';
+import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>Pixellab Ecom App</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-    <main>Hello World</main>
+
+      <Layout>
+        <main>this is main</main>
+      </Layout>
     </>
-  )
+  );
 }
