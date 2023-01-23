cb713658ee1b6a0092bc11c6bf9aebee4f7378c4
Pintilie <roxana.pintilie28@gmail.com>
2023-01-23 21:59:23 +0200

    clsaa

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,7 +1,21 @@
 import { Layout } from '@/layouts';
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://fakestoreapi.com/products')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setProducts(data);
+      });
+  }, [setProducts]);
+
   return (
     <>
       <Head>
@@ -11,7 +25,11 @@ export default function Home() {
       </Head>
 
       <Layout>
-        <main>this is main</main>
+        <main>
+          {products.map((product) => {
+            return <span key={product.id}>{product.title}</span>;
+          })}
+        </main>
       </Layout>
     </>
   );
